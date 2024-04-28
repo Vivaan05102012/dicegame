f208dbfa44fd8538d1a4c3329b3bf3148eb094d3
Vivaan Maheshwari <155884420+Vivaan05102012@users.noreply.github.com>
2024-04-28 11:50:14 +0100

    Create dice.js

---
+++ dice.js
@@ -0,0 +1,10 @@
+function dice1click() {
+  //random number 1 to 6
+  dice1value=Math.floor(Math.random() * 6) + 1;
+  console.log(dice1value)
+  document.getElementById("player1").innerHTML="Player 1's dice: "+dice1value;
+}
+function dice2click() {
+  //random number 1 to 6
+  
+}
